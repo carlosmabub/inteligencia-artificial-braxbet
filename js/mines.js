1ecb3516c079dedf4f07ed37d10c3010f45b6d5b
Carlos Felipe <carlosmabub@gmail.com>
2023-06-05 01:19:42 -0300

    primeiro commmit

---
+++ js/mines.js
@@ -0,0 +1,91 @@
+const button = document.getElementById('btn-hacker')
+button.addEventListener('click', () => t = 60 )
+button.addEventListener('click', sortear)
+
+t = 60
+function disabled(){
+    document.getElementById('contador').innerHTML = "Sinal valido por : " + t + " segundos"
+    t--
+    if(t < 0){
+        button.disabled = false
+    }else{
+        setTimeout(disabled, 1000);
+        button.disabled = true
+    }
+}
+
+function sortear(){
+    //reseta todas as imagens
+    document.getElementById(`1`).src = "../img/empty.png";
+    document.getElementById(`2`).src = "../img/empty.png";
+    document.getElementById(`3`).src = "../img/empty.png";
+    document.getElementById(`4`).src = "../img/empty.png";
+    document.getElementById(`5`).src = "../img/empty.png";
+    document.getElementById(`6`).src = "../img/empty.png";
+    document.getElementById(`7`).src = "../img/empty.png";
+    document.getElementById(`8`).src = "../img/empty.png";
+    document.getElementById(`9`).src = "../img/empty.png";
+    document.getElementById(`10`).src = "../img/empty.png";
+    document.getElementById(`11`).src = "../img/empty.png";
+    document.getElementById(`12`).src = "../img/empty.png";
+    document.getElementById(`13`).src = "../img/empty.png";
+    document.getElementById(`14`).src = "../img/empty.png";
+    document.getElementById(`15`).src = "../img/empty.png";
+    document.getElementById(`16`).src = "../img/empty.png";
+    document.getElementById(`17`).src = "../img/empty.png";
+    document.getElementById(`18`).src = "../img/empty.png";
+    document.getElementById(`19`).src = "../img/empty.png";
+    document.getElementById(`20`).src = "../img/empty.png";
+    document.getElementById(`21`).src = "../img/empty.png";
+    document.getElementById(`22`).src = "../img/empty.png";
+    document.getElementById(`23`).src = "../img/empty.png";
+    document.getElementById(`24`).src = "../img/empty.png";
+    document.getElementById(`25`).src = "../img/empty.png";
+    
+    //espera 10 segundos
+    setTimeout(() => {
+        disabled()
+        //guarda os numeros sorteados
+        var sorteados = []
+  
+        for(i = 0; i < 5; i++){
+            //escolher um numero de 1 a 25 e guarda na variavel
+            numSorte = Math.floor(Math.random()*25+1)
+            //verifica se o numero sorteado já esta na stream
+            //se não estiver, coloca no final da lista
+            if(sorteados.indexOf(numSorte) === -1){
+            sorteados.push(numSorte) 
+            //se estiver, ele conta novamente
+            }else{
+                i--
+            }
+        }
+        //troca as imagens pelas estrelas
+        document.getElementById(`${sorteados[0]}`).src = "../img/diamond.png";
+        document.getElementById(`${sorteados[1]}`).src = "../img/diamond.png";
+        document.getElementById(`${sorteados[2]}`).src = "../img/diamond.png";
+        document.getElementById(`${sorteados[4]}`).src = "../img/diamond.png";
+        document.getElementById(`${sorteados[5]}`).src = "../img/diamond.png";
+    }, 10000);
+    
+    setTimeout(() => {
+        document.getElementById('texto-hack').innerHTML = "Injetando Hack"
+        document.getElementById('texto-hack').style.color = 'red'
+    }, 1000);
+
+    setTimeout(() => {
+        document.getElementById('texto-hack').innerHTML = "Hackeando"
+        document.getElementById('texto-hack').style.color = 'yellow'
+    }, 5000);
+
+    setTimeout(() => {
+        document.getElementById('texto-hack').innerHTML = "Preparando o sinal"
+        document.getElementById('texto-hack').style.color = 'rgb(233, 255, 32)'
+    }, 7000);
+
+    setTimeout(() => {
+        document.getElementById('texto-hack').innerHTML = "Sinal pronto"
+        document.getElementById('texto-hack').style.color = 'rgb(63, 255, 38)'
+    }, 10000);
+}
+
---
+++ js/penalty.js
@@ -0,0 +1,74 @@
+const button = document.getElementById('btn-hacker1')
+button.addEventListener('click', () => t = 60 )
+button.addEventListener('click', sortear1)
+
+t = 60
+function contdown(){
+    document.getElementById('contador').innerHTML = "Sinal valido por : <br> " + t + " segundos"
+    t--
+    if(t < 0){
+        document.getElementById(`1`).src = "./img/bola.png";
+        document.getElementById(`2`).src = "./img/bola.png";
+        document.getElementById(`3`).src = "./img/bola.png";
+        document.getElementById(`4`).src = "./img/bola.png";
+        document.getElementById(`5`).src = "./img/bola.png";
+        document.getElementById('contador').innerHTML = "expirado"
+        document.getElementById('btn-hacker1').disabled = false
+    }else{
+        setTimeout(contdown, 1000)
+        document.getElementById('btn-hacker1').disabled = true
+    }
+}
+
+
+function sortear1(){  
+    
+    //guarda os numeros sorteados
+    //espera 10 segundos
+    setTimeout(() => {
+        contdown()
+        var sorteados = []
+        //sorteia quantos numeros serão sorteados    
+        var numDeVezes = Math.floor(Math.random()*4+1)
+        
+        for(i = 0; i < numDeVezes; i++){
+            numSorte = Math.floor(Math.random()*5+1)
+            //verifica se o numero sorteado já esta na stream
+            if(sorteados.indexOf(numSorte) === -1){
+            sorteados.push(numSorte) 
+            }else{
+                i--
+            }
+        }
+        document.getElementById(`${sorteados[0]}`).src = "./img/umx.png";
+        document.getElementById(`${sorteados[0]}`).style.width = '50px'
+        document.getElementById(`${sorteados[1]}`).src = "./img/doisx.png";
+        document.getElementById(`${sorteados[1]}`).style.width = '50px'
+        document.getElementById(`${sorteados[2]}`).src = "./img/tresx.png";
+        document.getElementById(`${sorteados[2]}`).style.width = '50px'
+        document.getElementById(`${sorteados[3]}`).src = "./img/quatrox.png";
+        document.getElementById(`${sorteados[3]}`).style.width = '50px'
+        document.getElementById(`${sorteados[4]}`).src = "./img/cincox.png";
+        document.getElementById(`${sorteados[4]}`).style.width = '50px'
+    }, 10000);
+    setTimeout(() => {
+        document.getElementById('texto-hack').innerHTML = "loading..."
+        document.getElementById('texto-hack').style.color = 'red'
+    }, 1000);
+
+    setTimeout(() => {
+        document.getElementById('texto-hack').innerHTML = "I. A. Analisando"
+        document.getElementById('texto-hack').style.color = 'yellow'
+    }, 5000);
+
+    setTimeout(() => {
+        document.getElementById('texto-hack').innerHTML = "Preparando o sinal"
+        document.getElementById('texto-hack').style.color = 'rgb(233, 255, 32)'
+    }, 7000);
+
+    setTimeout(() => {
+        document.getElementById('texto-hack').innerHTML = "Sinal pronto"
+        document.getElementById('texto-hack').style.color = 'rgb(63, 255, 38)'
+    }, 10000);
+
+}
